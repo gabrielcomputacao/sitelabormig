df8085cc806a294ae7feedfb04728b1f511f8a35
gabriel <gabrielcomputacao@outlook.com>
2024-09-21 10:51:40 -0300

    feat: criação de novas paginas de segmentos

--- js/behavior.js
+++ js/behavior.js
@@ -22,3 +22,21 @@ for (let index = 0; index < answers.length; index++) {
     topicAnswers[index].classList.toggle("topic-invisible");
   });
 }
+
+const atuacao = document.querySelector("#atuacao");
+const areas_atuacao = document.querySelector(".areas_atuacao");
+
+atuacao.addEventListener("click", (event) => {
+  areas_atuacao.classList.toggle("submenu-invi");
+  event.stopPropagation();
+});
+
+document.addEventListener("click", function (event) {
+  if (
+    !areas_atuacao.classList.contains("submenu-invi") &&
+    !areas_atuacao.contains(event.target) &&
+    event.target !== areas_atuacao
+  ) {
+    areas_atuacao.classList.add("submenu-invi");
+  }
+});
